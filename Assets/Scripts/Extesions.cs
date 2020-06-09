aa65f1a92fd31f9459fb1dd13c17ba98e3dd1656
Wagner Andrade <wagnerlinhares@bol.com.br>
2020-06-08 21:53:37 -0300

    First Commit

---
+++ Assets/Scripts/Extesions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extesions
+{
+    public static IEnumerator Move(this Transform t, Vector3 pos, float duration)
+    {
+        Vector3 direction = pos - t.position;
+        float distance = direction.magnitude;
+        direction.Normalize();
+
+        float startTime = 0;
+
+        if(startTime < duration)
+        {
+            float remainingDistance = (distance * Time.deltaTime) / duration;
+            t.position += direction * remainingDistance;
+            startTime += Time.deltaTime;
+            yield return null;
+        }
+        t.position = pos;
+    }
+
+    public static IEnumerator Scale(this Transform t, Vector3 scale, float duration)
+    {
+        Vector3 direction = scale - t.localScale;
+        float size = direction.magnitude;
+        direction.Normalize();
+
+        float startTime = 0;
+
+        if (startTime < duration)
+        {
+            float remainingDistance = (size * Time.deltaTime) / duration;
+            t.localScale += direction * remainingDistance;
+            startTime += Time.deltaTime;
+            yield return null;
+        }
+        t.localScale = scale;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# What's this project?
+It is a Mini game inspired in match 3 style games, like Candy Crush.
